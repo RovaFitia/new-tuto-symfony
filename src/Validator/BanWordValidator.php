4a9a407475a452ccad7c3cffa95e5175a1a3e712
Geek Code <rovafiti@email.com>
2024-02-23 20:43:33 +0300

    feat: validate a form and a component validator

---
+++ src/Validator/BanWordValidator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class BanWordValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        /* @var BanWord $constraint */
+
+        if (null === $value || '' === $value) {
+            return;
+        }
+
+        $value = strtolower($value) ;
+
+        foreach($constraint->banWords as $banWord) {
+            if(str_contains($value, $banWord)) {
+                // TODO: implement the validation here
+                $this->context->buildViolation($constraint->message)
+                    ->setParameter('{{ banword }}', $banWord)
+                    ->addViolation();
+            }
+        }
+
+
+    }
+}
